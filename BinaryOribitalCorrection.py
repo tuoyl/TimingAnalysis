1f06255a99ef8b61c9ea54a9e6bd81dfe10a4f8a
Youli Tuo <crazuni1993@gmail.com>
2020-06-29 08:55:36 +0800

    add Deeter et al. 1981 method for non-relativity binary

--- BinaryOribitalCorrection.py
+++ BinaryOribitalCorrection.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 
+from __future__ import division
 import numpy as np
 from astropy.io import fits
 
-__all__ = ["orbit_cor"]
+__all__ = ['orbit_cor_bt', 'orbit_cor_deeter']
 
 #correcting_method = ["Reggio", "Sanna", "DT"]
+correcting_method = ["BT", "Deeter"]
 
 
-def orbit_cor(t, Porb, asini, e, omega, Tw):
+def orbit_cor_bt(t, Porb, asini, e, omega, Tw, gamma):
     """
     use numerical method to solve Kepler equation and calculate delay
+    BT model (Blandford & Teukolsky, 1976)
 
     Parameters
     -----------------
@@ -31,6 +34,9 @@ def orbit_cor(t, Porb, asini, e, omega, Tw):
 
     Tw : float 
         The epoch of periastron passage (in units of seconds, same time system with parameter t)
+
+    gamma ; float
+        the coefficient measures the combined effect of gravitational redshift and time dilation
         
     Returns 
     -------------
@@ -39,6 +45,7 @@ def orbit_cor(t, Porb, asini, e, omega, Tw):
 
     """
     x = asini
+    gamma = 0 
 
     if e == 0:
         E = 2*np.pi*(t-Tw)/Porb;
@@ -60,7 +67,7 @@ def orbit_cor(t, Porb, asini, e, omega, Tw):
     
     #calculate time delay by orbit
     #factor1
-    factor1 = x*np.sin(omega)*(np.cos(E)-e) + (x*np.cos(omega)* ((1-e**2)**0.5) )*np.sin(E)
+    factor1 = x*np.sin(omega)*(np.cos(E)-e) + (x*np.cos(omega)* ((1-e**2)**0.5) + gamma )*np.sin(E)
     #factor2
     factor2 = 1- (2*np.pi/Porb)* (x*np.cos(omega)*((1-e**2)**0.5)-x*np.sin(omega)*np.sin(E)) * (1-e*np.cos(E))**(-1)
     factor = factor1 * factor2
@@ -68,6 +75,47 @@ def orbit_cor(t, Porb, asini, e, omega, Tw):
     print(factor)
     return new_t
 
+
+def orbit_cor_deeter(time, Porb, asini, e, omega, T):
+    """
+    Correct the photon arrival times to the photon emission time
+    Deeter model (see, e.g., Deeter et al. 1981)
+
+    Parameters
+    -----------------
+    time : array-like
+        The time series before binary correction
+
+    Porb : float
+        The period of binary motion (in units of second)
+
+    asini : float
+        Projected semi-major orbital axis (in units of light-sec)
+
+    e : float 
+        The orbital eccentricity
+
+    omega : float
+        Longitude of periastron
+
+    T : float 
+        The epoch of ascending node passage (in units of seconds, same time system with parameter t)
+
+    Returns 
+    -------------
+    t_em : array-like
+        The photon emission time
+
+    """
+    A = asini
+    mean_anomaly = 2*np.pi*(time-T)/Porb
+
+    term1 = np.sin(mean_anomaly + omega) 
+    term2 = (e/2)*np.sin(2*mean_anomaly + omega)
+    term3 = (-3*e/2)*np.sin(omega)
+
+    t_em = time - A * (term1 + term2 + term3)
+    return t_em
 
 if __name__ == "__main__":
     hdulist = fits.open("./testdata/P021100601401_he_screen.fits")
